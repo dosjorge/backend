76b25b1734dd871b332099f223f65f0e41e40318
dosjorge <66338435+dosjorge@users.noreply.github.com>
2021-07-29 00:25:25 -0400

    Corrije regresión por cambio en configuración

--- models/index.js
+++ models/index.js
@@ -8,24 +8,13 @@ const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+const sequelize = new Sequelize(config.url, config);
 
 fs.readdirSync(__dirname)
-  .filter((file) => {
-    return (
+  .filter(
+    (file) =>
       file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-    );
-  })
+  )
   .forEach((file) => {
     const model = require(path.join(__dirname, file))(
       sequelize,
